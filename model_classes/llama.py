053be0006fcb6f7169507e83bff9f98db48c5e0c
vasiliskarlis <vasilis.karlis@student.uva.nl>
2024-09-18 19:43:08 +0200

    classes structure

---
+++ model_classes/llama.py
@@ -0,0 +1,18 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from typing import List, Dict
+import torch
+from huggingface_hub import login
+
+
+
+class LLama3:
+
+    def __init__(self):
+        
+        self.model_id ='path'
+
+    def inference(self, input):
+
+        output= 0
+
+        return output
